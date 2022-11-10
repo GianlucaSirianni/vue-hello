e1f35e49c240e546e99db84804bbd21ea89d14d1
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-10 15:58:46 +0100

    esercizio completo con immagine

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue
+(
+    {
+        el: '#root',
+        data: 
+        {
+            title: 'This is a Title, and a little further down you can see the image',
+            img: './img/pokeball.png'
+        }
+        
+    }
+);
